33953cb233d7daccf0b91ce28bbe2aa1c9cd3521
Murvin Bhantooa <mbhantooa@gmail.com>
2018-10-07 10:24:54 +0100

    Created an api response wrapper.

---
+++ app/src/main/java/com/rezolve/gistscanner/api/GistApiResponse.java
@@ -0,0 +1,44 @@
+package com.rezolve.gistscanner.api;
+
+import android.support.annotation.NonNull;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Abstraction to hold an API service call response. There can be a response R or an exception of
+ * type E. The subclasses will decide the appropriate types.
+ *
+ * @param <R> the API documented response type
+ * @param <E> a convenient exception type
+ */
+public class GistApiResponse<R, E> {
+    private final R response;
+    private final E exception;
+
+    public GistApiResponse(@Nullable R response,
+                           @Nullable E exception) {
+        this.response = response;
+        this.exception = exception;
+
+        if (response == null ^ exception == null) {
+            throw new IllegalArgumentException("At least one of R or E should not be null.");
+        }
+    }
+
+    public R getResponse() {
+        return response;
+    }
+
+    public E getException() {
+        return exception;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "GistApiResponse{" +
+                "response=" + response +
+                ", exception=" + exception +
+                '}';
+    }
+}
